e79e8b9f9053d35d40dca6f84dc3f6fd8265e3ef
Damain Ramsajan <dpramsajan@gmail.com>
2021-02-09 00:19:50 +0100

    Navigation setup complete

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {NavigationContainer, StackActions} from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './src/screens/Home';
+
+const Stack = createStackNavigator ();
+
+const globalScreenOptions = {
+  headerStyle: {backgroundColor: "#2A4E70"},
+  headerTitleStyle: {color: "white"},
+  headerTintColor: "white",
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Dami is the dan!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <StatusBar style="auto" />
+        <Stack.Navigator screenOptions = {globalScreenOptions}>
+        <Stack.Screen name = "Home" component = {Home} options = {{title: "Stretch Your Imagination"}}/>      
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 }
 
